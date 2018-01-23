419638b4b0d2aa74866af92af94ffc816576b64f
lucatonile <jaamongo@gmail.com>
2018-01-23 21:27:40 +0100

    pathen i android/local.properties måste ändras om vi använder denna

--- client/App.js
+++ client/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import SampleBrowser from './src/components/samplebrowser';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    StatusBar.setHidden(true);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <SampleBrowser>eyyy</SampleBrowser>
       </View>
     );
   }
@@ -15,9 +18,6 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
